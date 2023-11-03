41bc1d4dc67dee63e5eb2887327ce8dab68a52d1
markusbenjamin <markus.benjamin@gmail.com>
2023-11-03 18:30:57 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
 
 function draw() {
     // Your drawing code goes here
-    fill(255, 0, 0); // Fill with red color
+    fill(255, 125, 0); // Fill with red color
     noStroke();
     ellipse(mouseX, mouseY, 50, 50); // Draw an ellipse at the position of the mouse
 }
