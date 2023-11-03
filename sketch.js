a64ea0696a7cf6390860859f591b08997f1de7b9
markusbenjamin <56760290+markusbenjamin@users.noreply.github.com>
2023-11-03 18:00:03 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+// p5.js sketch
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(100); // A gray background: you can change it
+}
+
+function draw() {
+    // Your drawing code goes here
+    fill(255, 0, 0); // Fill with red color
+    noStroke();
+    ellipse(mouseX, mouseY, 50, 50); // Draw an ellipse at the position of the mouse
+}
+
+// This function automatically resizes the canvas when the window is resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
