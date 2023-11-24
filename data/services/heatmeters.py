d84abc36ee09566ac97685c481338f40f39a2529
markusbenjamin <markus.benjamin@gmail.com>
2023-11-24 21:41:26 +0100

    Better cropping

--- data/services/heatmeters.py
+++ data/services/heatmeters.py
@@ -33,7 +33,7 @@ def capture_images(capture_duration, capture_frequency):
         subprocess.run(['fswebcam', '-r', '1280x720', '--no-banner', image_filename])
         print(f'Captured image {image_filename}.')
 
-        crop_cycles(image_filename)
+        # crop_cycles(image_filename)
 
         # Wait for the next capture
         time.sleep(capture_frequency)
@@ -47,10 +47,19 @@ def crop_cycles(img_path):
     ]
 
     with Image.open(img_path) as img:
-        img = img.transpose(Image.ROTATE_90)
-        for cycle in range(1,5):
+        # Rotate with antialiasing
+        img = img.rotate(90, resample=Image.BICUBIC, expand=True)
+
+        for cycle in range(1, 5):
             try:
-                img.crop(crop_rectangles[cycle-1]).save(f"{img_path[0:-4]}_{cycle}.jpg")
+                # Create a high-quality intermediate image
+                cropped_img = img.crop(crop_rectangles[cycle - 1])
+                cropped_img_path = f"{img_path[0:-4]}_{cycle}.png"  # Save as PNG
+                cropped_img.save(cropped_img_path, format='PNG')
+
+                # Optionally convert to JPEG with high quality
+                # cropped_img.save(cropped_img_path.replace('.png', '.jpg'), format='JPEG', quality=95)
+
                 print(f"\tCycle {cycle} successfully cropped.")
             except Exception as e:
                 print(f"\tCouldn't crop cycle {cycle} due to {e}.")
