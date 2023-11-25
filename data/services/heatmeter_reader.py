b8e5a0770df346bd2038160289c47f4e0dfc2230
benjaminmarkus <markus.benjamin@gmail.com>
2023-11-26 00:43:29 +0100

    Minor fixes in heatmeter reader

--- data/services/heatmeter_reader.py
+++ data/services/heatmeter_reader.py
@@ -332,8 +332,7 @@ def do_ocr_on_cycles(cycle_crops):
  
 
 if __name__ == "__main__":
-    #image_filename = capture_image()
-    image_filename = "img.jpg"
+    image_filename = capture_image()
 
     cycle_crops = crop_cycles(image_filename)
 
